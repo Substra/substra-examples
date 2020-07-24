51b5e3f0968fe57822fa742e6b854db4b710808d
Fabien-GELUS <fabien.gelus@insa-lyon.fr>
2020-07-24 17:04:23 +0200

    set sdk 0.6.0 commands

--- mnist/scripts/add_train_algo_cnn.py
+++ mnist/scripts/add_train_algo_cnn.py
@@ -7,7 +7,7 @@ import substra
 current_directory = os.path.dirname(__file__)
 assets_directory = os.path.join(current_directory, '../assets')
 
-client = substra.Client.from_config_file(profile_name="node-1")
+client = substra.Client(profile_name="node-1")
 
 ALGO_KEYS_JSON_FILENAME = 'algo_cnn_keys.json'
 
