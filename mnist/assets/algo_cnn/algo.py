536dc68e5364c77b4fcd2cfa3cf42c615dae7633
Fabien-GELUS <fabien.gelus@insa-lyon.fr>
2020-07-29 17:59:33 +0200

    update algo with tensorflow

--- mnist/assets/algo_cnn/algo.py
+++ mnist/assets/algo_cnn/algo.py
@@ -1,10 +1,9 @@
 import substratools as tools
 import os
-import keras
-from keras import backend as K
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
+import numpy as np
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras import layers
 
 # input image dimensions
 img_rows, img_cols = 28, 28
@@ -14,18 +13,10 @@ num_classes = 10
 class Algo(tools.algo.Algo):
     def _normalize_X(self, X):
 
-        if K.image_data_format() == "channels_first":
-            X = X.reshape(X.shape[0], 1, img_rows, img_cols)
-
-            input_shape = (1, img_rows, img_cols)
-        else:
-            X = X.reshape(X.shape[0], img_rows, img_cols, 1)
-
-            input_shape = (img_rows, img_cols, 1)
-
-        X = X.astype("float32")
-
-        X /= 255
+        # Scale images to the [0, 1] range
+        X = X.astype("float32") / 255
+        # Make sure images have shape (28, 28, 1)
+        X = np.expand_dims(X, -1)
 
         print(f"X shape: {X.shape}")
         print(f"{X.shape[0]} samples")
@@ -34,27 +25,25 @@ class Algo(tools.algo.Algo):
 
     def _init_new_model(self):
 
-        model = Sequential()
-        model.add(
-            Conv2D(32, kernel_size=(3, 3), activation="relu", input_shape=input_shape)
+        model = keras.Sequential(
+            [
+                keras.Input(shape=input_shape),
+                layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+                layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+                layers.MaxPooling2D(pool_size=(2, 2)),
+                layers.Flatten(),
+                layers.Dropout(0.5),
+                layers.Dense(num_classes, activation="softmax"),
+            ]
         )
-        model.add(Conv2D(64, (3, 3), activation="relu"))
-        model.add(MaxPooling2D(pool_size=(2, 2)))
-        model.add(Dropout(0.25))
-        model.add(Flatten())
-        model.add(Dense(128, activation="relu"))
-        model.add(Dropout(0.5))
-        model.add(Dense(num_classes, activation="softmax"))
 
-        model.compile(
-            loss=keras.losses.categorical_crossentropy,
-            optimizer=keras.optimizers.Adadelta(),
-            metrics=["accuracy"],
-        )
+        model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
         return model
 
     def train(self, X, y, models, rank):
+        
         X = self._normalize_X(X)
 
         # convert class vectors to binary class matrices
@@ -67,24 +56,25 @@ class Algo(tools.algo.Algo):
             y,
             batch_size=128,
             epochs=1,
-            verbose=1,
+            validation_split=0.1,
         )
 
         return model
 
     def predict(self, X, model):
         X = self._normalize_X(X)
-        y_pred = model.predict_classes(X, verbose=0)
+        y_pred = np.argmax(model.predict(X), axis=-1)
         return y_pred
 
     def load_model(self, path):
-        with open(path, "rb") as f:
-            return keras.models.load_model(f)
+        os.rename(path, path+'.h5')
+        model = tensorflow.keras.models.load_model(path+'.h5')
+        os.rename(path+'.h5', path)
+        return model
 
     def save_model(self, model, path):
-        os.makedirs(os.path.dirname(path), exist_ok=True)
-        with open(path, "wb") as f:
-            model.save(f)
+        model.save(path+'.h5')
+        os.rename(path+'.h5', path)
 
 
 if __name__ == "__main__":
